f05f6bbc093f3fe821e6551833f08dc357211f14
Your Name <2093434422@qq.com>
2024-08-13 21:01:18 +0800

    last

---
+++ text/1.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         nana
+// @namespace    org.sessx6cf.etw360ff
+// @version      2024-08-11
+// @description  hello，world
+// @author       joke
+// @match        https://teacher.ewt360.com/ewtbend/bend/index/index.html
+// @icon         https://greasyfork.org/vite/assets/blacklogo96-CxYTSM_T.png
+// @grant        none
+// ==/UserScript==2/favicons?sz=64&domain=github.com
+
+(function() {
+    'use strict';
+
+    console.log('hello,world');
+    
+})();
